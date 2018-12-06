91a7c9a53508d2b9ceb8d8c15903e526eea3ca4a
Orhan Erbas <orhanerba@icloud.com>
2018-12-06 23:15:47 +0300

    firebase setting added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from "redux";
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class Index extends Component{
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyAWriKnitfLT-KR-BXBCYJD8G2RTh9Sa4U',
+            authDomain: 'manager-87c87.firebaseapp.com',
+            databaseURL: 'https://manager-87c87.firebaseio.com',
+            projectId: 'manager-87c87',
+            storageBucket: 'manager-87c87.appspot.com',
+            messagingSenderId: '365809451172'
+        };
+        firebase.initializeApp(config);
+    }
+
+    render() {
+        return (
+          <Provider store={createStore(reducers)}>
+              <View>
+                  <Text> Hi! </Text>
+              </View>
+          </Provider>
+        );
+    }
+
+}
+
+export default Index;

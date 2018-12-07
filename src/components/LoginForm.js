9721b5e384e2364e579fd4b0ab2af71dcba1ae9e
Orhan Erbas <orhanerba@icloud.com>
2018-12-08 01:03:00 +0300

    showed error message from auth

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from "../actions";
 import { Card, CardSection, Input, Button } from './common';
@@ -14,6 +15,17 @@ class LoginForm extends Component{
     onButtonPress(){
         const { email, password } = this.props;
         this.props.loginUser({ email, password });
+    }
+    renderError(){
+        if (this.props.error) {
+            return (
+              <View style={{ backgroundColor: 'white' }}>
+                  <Text style={styles.errorTextStyles}>
+                      {this.props.error}
+                  </Text>
+              </View>
+            );
+        }
     }
     render() {
         return (
@@ -37,6 +49,8 @@ class LoginForm extends Component{
                     />
                 </CardSection>
 
+                {this.renderError()}
+
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
                         Login
@@ -47,10 +61,19 @@ class LoginForm extends Component{
     }
 }
 
+const styles = {
+    errorTextStyles:{
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
+
 const mapStateToProps = state => {
   return {
       email: state.auth.email,
-      password: state.auth.password
+      password: state.auth.password,
+      error: state.auth.error
   }
 };
 

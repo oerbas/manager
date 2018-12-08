bb500d8edb84518a22f3b0ba52ff1e5537c58ad4
Orhan Erbas <orhanerba@icloud.com>
2018-12-08 16:46:24 +0300

    employee actions created

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, Button, CardSection, Input } from "./common";
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <View>
-                <Text>Employee Form</Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Name"
+                        placeholder="Orhan"
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        label="Phone"
+                        placeholder="555-555-55-55"
+                    />
+                </CardSection>
+
+                <CardSection>
+                </CardSection>
+
+                <CardSection>
+                    <Button>
+                        Create
+                    </Button>
+                </CardSection>
+            </Card>
         );
     }
 }

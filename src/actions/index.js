4636de1c2f0db18e41d289420c94ac52391215c9
Orhan Erbas <orhanerba@icloud.com>
2018-12-08 16:20:56 +0300

    added employeeList and right button on header

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firabase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from "./types";
 
 export const emailChanged = (text) => {
@@ -33,4 +34,5 @@ const loginUserFail = (dispatch, user) => {
 };
 const loginUserSuccess = (dispatch, user) => {
     dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+    Actions.main();
 };

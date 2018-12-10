89d11a22a0ed417a87721fbd7ba2688f27e6623e
Orhan Erbas <orhanerba@icloud.com>
2018-12-10 23:32:06 +0300

    update employee done

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate } from "../actions";
+import { employeeUpdate, employeeSave } from "../actions";
 import { Card, CardSection, Button } from "./common";
 
 class EmployeeEdit extends Component {
@@ -13,6 +13,7 @@ class EmployeeEdit extends Component {
     }
     onButtonPress(){
         const { name, phone, shift } = this.props;
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
     }
     render() {
         return (
@@ -33,4 +34,4 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate }) (EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave }) (EmployeeEdit);

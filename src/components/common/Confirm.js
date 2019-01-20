5e74f6940a743b69b6ecbc32aa8e0db6fecb534e
Orhan Erbas <orhanerba@icloud.com>
2019-01-20 21:04:14 +0300

    modal continued

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -4,6 +4,7 @@ import { CardSection } from "./CardSection";
 import { Button } from "./Button";
 
 const Confirm = ({ children, visible, onAccept, onDecline }) => {
+    const { containerStyle, textStyle, cardSectionStyle } = style;
     return (
       <Modal
         visible={visible}
@@ -11,9 +12,9 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
         animationType="slide"
         onRequestClose={() => {}}
       >
-          <View>
-              <CardSection>
-                  <Text>{children}</Text>
+          <View style={containerStyle}>
+              <CardSection style={cardSectionStyle}>
+                  <Text style={textStyle}>{children}</Text>
               </CardSection>
               <CardSection>
                   <Button onPress={onAccept}>YES</Button>
@@ -22,6 +23,24 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
           </View>
       </Modal>
     );
+};
+
+const style = {
+  cardSectionStyle: {
+      justifyContent: 'center'
+  },
+  textStyle: {
+      flex: 1,
+      fontSize: 18,
+      textAlign: 'center',
+      lineHeight: 40
+  },
+  containerStyle: {
+      backgroundColor: 'rgba(0, 0, 0, 0.75)',
+      position: 'relative',
+      flex: 1,
+      justifyContent: 'center'
+  }
 };
 
 export { Confirm };

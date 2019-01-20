3f38fe51871bca9b2b6f69ce57b14013d6b4c559
Orhan Erbas <orhanerba@icloud.com>
2019-01-20 21:13:37 +0300

    adding delete actions

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -45,3 +45,15 @@ export const employeeSave = ({ name, phone, shift, uid }) => {
             .then(() => console.log("saved!"))
     }
 };
+
+export const employeeDelete = ({ uid }) => {
+    const { currentUser } = firebase.auth();
+
+    return () => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+            .remove()
+            .then(() => {
+                Actions.employeeList({ type: 'reset' });
+            });
+    };
+};
--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -22,6 +22,12 @@ class EmployeeEdit extends Component {
 
         Communications.text(phone, `Your upcomming shift is on ${shift}`);
     }
+    onAccept(){
+
+    }
+    onDecline(){
+        this.setState({ showModal: false });
+    }
     render() {
         return (
            <Card>
@@ -44,6 +50,8 @@ class EmployeeEdit extends Component {
 
                <Confirm
                    visible={this.state.showModal}
+                   onAccept={this.onAccept.bind(this)}
+                   onDecline={this.onDecline.bind(this)}
                >
                    Are you sure you want to delete this?
                </Confirm>

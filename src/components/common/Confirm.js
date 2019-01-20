4711bf8c2430af2a16bb8981661f0bb8ad68d74a
Orhan Erbas <orhanerba@icloud.com>
2019-01-20 20:44:52 +0300

    modal continued

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,8 +3,25 @@ import { Text, View, Modal } from "react-native";
 import { CardSection } from "./CardSection";
 import { Button } from "./Button";
 
-const Confirm = () => {
-
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+    return (
+      <Modal
+        visible={visible}
+        transparent
+        animationType="slide"
+        onRequestClose={() => {}}
+      >
+          <View>
+              <CardSection>
+                  <Text>{children}</Text>
+              </CardSection>
+              <CardSection>
+                  <Button onPress={onAccept}>YES</Button>
+                  <Button onPress={onDecline}>NO</Button>
+              </CardSection>
+          </View>
+      </Modal>
+    );
 };
 
 export { Confirm };
